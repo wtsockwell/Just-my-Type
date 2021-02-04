c1fe5546441482f0feac8a367a7037ee99aa9ccb
William Taylor Sockwell <wtsockwell@yahoo.com>
2021-02-04 09:22:07 -0600

    Added button highlighting and fixed issue with board changing case after pressing a button

--- app.js
+++ app.js
@@ -1,22 +1,39 @@
-$(document).ready(function(){
+$(document).ready(function () {
     //Hiding the uppercase board and revealing it when the shift key is held down
     $("#keyboard-upper-container").hide();
-    
-    let shiftArea = $("body")
-    shiftArea.keydown(function(event){
-        if(event.key == "Shift"){
+
+    let gameSpace = $("body")
+    gameSpace.keydown(function (event) {
+        let character = event.key
+
+        if (event.shiftKey == true) {
             $("#keyboard-lower-container").hide();
             $("#keyboard-upper-container").show();
+            //Ability to keep the board properly selected as well as highlight the selected character
+            $(`.well#${character.charCodeAt(0)}`).css("background-color", "skyblue")
+            if (event.key == "Shift") {
+                $(".well#83").css("background-color", "#f5f5f5")
+            } else {
+                $(`.well#${character.charCodeAt(0)}`).css("background-color", "skyblue")
+            }
+            gameSpace.keyup(function () {
+                $(`.well#${character.charCodeAt(0)}`).css("background-color", "#f5f5f5")
+            })
+        } else {
+            $(`.well#${character.charCodeAt(0)}`).css("background-color", "skyblue")
+            gameSpace.keyup(function () {
+                $(`.well#${character.charCodeAt(0)}`).css("background-color", "#f5f5f5")
+            })
         }
     })
-    shiftArea.keyup(function(){
-        $("#keyboard-upper-container").hide();
-        $("#keyboard-lower-container").show();
+    gameSpace.keyup(function(e){
+        if(e.key == "Shift"){
+            $("#keyboard-upper-container").hide();
+            $("#keyboard-lower-container").show();
+        }
     })
 
-    //Highlighting the keys when they are pressed
-    $(".key").keydown(function (e) { 
-        
-    });
 
+
+    
 })
